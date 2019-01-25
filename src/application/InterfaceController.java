075bc2c67966ed555e87ea0ce210a938ecbae9f9
Marc Rettenbacher <tyratox.rettenbacher@gmail.com>
2019-01-25 18:13:25 +0100

    added win checking

--- src/application/InterfaceController.java
+++ src/application/InterfaceController.java
@@ -7,10 +7,14 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class InterfaceController {
 	@FXML
 	private AnchorPane anchor;
+	
+	@FXML
+	private Text turn_text;
 	
 	@FXML //needed to be recognized by fxml file
 	private void fieldImageAction(MouseEvent event) {
@@ -18,22 +22,29 @@ public class InterfaceController {
 		int idrow = Character.getNumericValue(((Node) event.getSource()).getId().charAt(1));
 		int idcolumn = Character.getNumericValue(((Node) event.getSource()).getId().charAt(2));
 		int turn = Main.getTurn(); //0 means cross, 1 means circle
+		//circle's turn (Player2)
 		if(turn==1 && fieldMatrix[idrow][idcolumn]==0) {
 			Image image = new Image("file:tile_circle.png"); //sets the new image
 			//gets the current image that is clicked on and performs the desired action on it
 			((ImageView) event.getSource()).setImage(image);
+			//update current turn indicator to next player
+			turn_text.setText("Current turn: PLAYER1");
 			Main.update(idrow, idcolumn, turn);
 		}
+		//cross' turn (Player1)
 		else if(turn == 0 && fieldMatrix[idrow][idcolumn]==0) {
 			Image image = new Image("file:tile_cross.png"); //sets the new image
 			//gets the current image that is clicked on and performs the desired action on it
 			((ImageView) event.getSource()).setImage(image);
+			//update current turn indicator to next player
+			turn_text.setText("Current turn: PLAYER2");
 			Main.update(idrow, idcolumn, turn);
 		}
 	}
 	
 	public void resetField(ActionEvent event) {
 		Main.resetLogic();
+		turn_text.setText("Current turn: PLAYER1");
 		Image resimg = new Image("file:tile.png");
 		for(Node node : anchor.getChildren()) {
 			if(node instanceof ImageView) {
--- src/application/Main.java
+++ src/application/Main.java
@@ -40,18 +40,74 @@ public class Main extends Application {
 	//0 means cross turn -> Matrix with 1
 	//1 means circle turn -> Matrix with 2
 	public static void update(int row, int column, int currentTurn) {
-		
+		//update turn and matrix	
 		if(currentTurn==0) {
 			turn = 1;
 			fieldMatrix[row][column] = 1;
 		}
-		else {
+		else if(currentTurn==1) {
 			turn = 0;
 			fieldMatrix[row][column] = 2;
 		}
+		//check if the game has ended
+		if(gameDone()) {
+			System.out.println("The game is over");
+			//InterfaceController.resetField();
+		}
+	}
+	
+	public static boolean gameDone(){
+		//check if Matrix is full
+		boolean done = false;
+		int zeroCount = 0;
+		for(int i=0; i<fieldMatrix.length; i++){
+			for (int j = 0; j < fieldMatrix[i].length; j++) {
+				if (fieldMatrix[i][j] == 0) {
+					zeroCount += 1;
+				}
+			}
+		}
+		
+		if(zeroCount == 0) { //no 0's -> All fields have been filled out, game over
+			done = true;
+		}
+		
+		else { //check if a wincondition happened for either player
+			int winner = checkWin();
+			if(winner==1 || winner==2) {
+				done = true;
+			}
+		}
+		return done;
+	}
+	
+	public static int checkWin() {
+		for (int x = 1; x <= 2; x++) {
+			for (int i = 0; i < fieldMatrix.length; i++) {
+				//check if a column win happened
+				if(fieldMatrix[i][0] == x && fieldMatrix[i][1] == x && fieldMatrix[i][2] == x) {
+					return 1;
+				}
+				//check if a row win happened
+				else if(fieldMatrix[0][i] == x && fieldMatrix[1][i] == x && fieldMatrix[2][i] == x) {
+					return 1;
+				}
+			}
+			//check if diagonal win happened
+			if(fieldMatrix[0][0] == x && fieldMatrix[1][1] == x && fieldMatrix[2][2] == x) {
+				return 1;
+			}
+			else if(fieldMatrix[0][2] == x && fieldMatrix[1][1] == x && fieldMatrix[2][0] == x) {
+				return 1;
+			}
+		}
+		
+		//if no win happened
+		return 0;
 	}
 	
 	public static void resetLogic() {
 		fieldMatrix = new int[3][3];
+		turn=0;
 	}
 }
